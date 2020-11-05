f2de4bf1c620c3c5090686f572de64c069f6c57b
fviramontes8 <kuolemajarvi@yahoo.com>
2020-11-04 22:25:13 -0700

    Finished queue class, will work on main next

--- GeneralQueue.hpp
+++ GeneralQueue.hpp
@@ -137,4 +137,81 @@ class Queue {
 			return false;
 		}
 
+		Queue<Q> &operator=(const Queue<Q> &q) {
+			if (q.empty()) {
+				if (size() == 1) {
+					delete _data;
+				}
+				else {
+					delete [] _data;
+				}
+				_size = 0;
+				_data = nullptr;
+			}
+			else if (q.size() == 1) {
+				Q q_value = *q._data;
+				if (size() == 1) {
+					delete _data;
+				}
+				else {
+					delete [] _data;
+				}
+				_data = new Q(q_value);
+				_size = 1;
+			}
+			else {
+				size_t q_size = q.size();
+				Q q_data_copy = new Q[q_size];
+				size_t i;
+				for (i=0; i<q_size; i++) {
+					*(data_copy + i) = *(q._data + i);
+				}
+
+				if (size() == 1) {
+					delete _data;
+				}
+				else {
+					delete [] _data;
+				}
+
+				_data = new Q[q_size];
+				for (i=0; i<q_size; i++) {
+					*(_data + i) = *(data_copy + i);
+				}
+
+				_size = q_size;
+				delete [] q_data_copy;
+			}
+			return *this;
+		}
+
+		template <typename T>
+		friend std::ostream &operator<<(std::ostream &out, const Queue<T> &q) {
+				for (i=0; i<q.size(); i++) {
+					out << *(q._data + i) << ' ';
+				}
+				return out;
+		}
+
+		template <typename T>
+		friend bool operator==(const Queue<T> &a, const Queue<T> &b);
+
+		template <typename T>
+		friend bool operator!=(const Queue<T> &a, const Queue<T> &b) {
+			return !(a==b);
+		}
 };
+
+template <typename T>
+bool operator==(const Queue<T> &a, const Queue<T> &b) {
+	if (a.size() != b.size()) {
+		return false;
+	}
+
+	for (i=0; i<b.size(); i++) {
+		if ( (*(a._data + i) != (*(b._data + i))) {
+			return false;
+		}
+	}
+	return true;
+}
